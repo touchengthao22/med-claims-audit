90db9c29565cdd3c6a5c8f24346c007203d220bf
touchengthao22 <thaoto@oregonstate.edu>
2026-04-07 09:59:41 -0700

    updated query

--- sql/queries.sql
+++ sql/queries.sql
@@ -55,3 +55,24 @@ from staging_tbl
 where
 	LINE_NCH_PMT_AMT_1  > LINE_ALOWD_CHRG_AMT_1 ;
 
+-- check if there are any anomalies in allowable charge
+with avg_tbl as (
+	select 
+		DESYNPUF_ID,
+		CLM_ID,
+		CLM_FROM_DT,
+		ICD9_DGNS_CD_1,
+		HCPCS_CD_1,
+		LINE_NCH_PMT_AMT_1,
+		LINE_ALOWD_CHRG_AMT_1,
+		AVG(nullif(LINE_ALOWD_CHRG_AMT_1, 0)) over (partition by HCPCS_CD_1) as avg_allwd
+	from staging_tbl
+	where HCPCS_CD_1 is not null
+)
+
+-- This query is checking for any allowable charge that is more than 50% higher than avg for treatment
+select *
+from avg_tbl
+where LINE_ALOWD_CHRG_AMT_1 >= avg_allwd * 1.5;
+
+
